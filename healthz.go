7e954cdd54474c1312f5d8cccf14fc627d5000e9
Rahul Chitkara <rahulc@entity1.com>
2025-12-10 18:55:06 +1100

    Minor changes to function signiture for healthz handler

--- healthz.go
+++ healthz.go
@@ -2,8 +2,8 @@ package main
 
 import "net/http"
 
-func healthz(res http.ResponseWriter, req *http.Request) {
-	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	res.WriteHeader(200)
-	res.Write([]byte("OK"))
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
